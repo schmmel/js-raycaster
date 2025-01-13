e63fa329c2c2250dbd83cfd8084967f0a6142ced
schimmel <sylvienuijtemans@gmail.com>
2025-01-13 15:49:39 +0100

    move comments

--- js/movement.js
+++ js/movement.js
@@ -14,6 +14,7 @@ function movement() {
     let moveSpeed = deltaTime * 5;
     let rotateSpeed = deltaTime * 2.5;
     // multiplying moveSpeed by diagonalPenalty results in the distance traveled diagonally to be the same as going straight because pythagoras theory
+    // value might need to be adjusted if i change moveSpeed
     let diagonalPenalty = 0.708;
 
     if ((keys["KeyW"] && keys["KeyA"] && !keys["KeyD"]) ||
@@ -43,23 +44,23 @@ function movement() {
         if (map[Math.floor(playerY + dirX * moveSpeed)][Math.floor(playerX)] === 0) { playerY += dirX * moveSpeed; }
     }
 
-    if (keys["ArrowLeft"]) {
-        let oldDirX = dirX;
-        dirX = dirX * Math.cos(-rotateSpeed) - dirY * Math.sin(-rotateSpeed);
-        dirY = oldDirX * Math.sin(-rotateSpeed) + dirY * Math.cos(-rotateSpeed);
-        let oldPlaneX = planeX;
-        planeX = planeX * Math.cos(-rotateSpeed) - planeY * Math.sin(-rotateSpeed);
-        planeY = oldPlaneX * Math.sin(-rotateSpeed) + planeY * Math.cos(-rotateSpeed);
-    }
+    // if (keys["ArrowLeft"]) {
+    //     let oldDirX = dirX;
+    //     dirX = dirX * Math.cos(-rotateSpeed) - dirY * Math.sin(-rotateSpeed);
+    //     dirY = oldDirX * Math.sin(-rotateSpeed) + dirY * Math.cos(-rotateSpeed);
+    //     let oldPlaneX = planeX;
+    //     planeX = planeX * Math.cos(-rotateSpeed) - planeY * Math.sin(-rotateSpeed);
+    //     planeY = oldPlaneX * Math.sin(-rotateSpeed) + planeY * Math.cos(-rotateSpeed);
+    // }
 
-    if (keys["ArrowRight"]) {
-        let oldDirX = dirX;
-        dirX = dirX * Math.cos(rotateSpeed) - dirY * Math.sin(rotateSpeed);
-        dirY = oldDirX * Math.sin(rotateSpeed) + dirY * Math.cos(rotateSpeed);
-        let oldPlaneX = planeX;
-        planeX = planeX * Math.cos(rotateSpeed) - planeY * Math.sin(rotateSpeed);
-        planeY = oldPlaneX * Math.sin(rotateSpeed) + planeY * Math.cos(rotateSpeed);
-    }
+    // if (keys["ArrowRight"]) {
+    //     let oldDirX = dirX;
+    //     dirX = dirX * Math.cos(rotateSpeed) - dirY * Math.sin(rotateSpeed);
+    //     dirY = oldDirX * Math.sin(rotateSpeed) + dirY * Math.cos(rotateSpeed);
+    //     let oldPlaneX = planeX;
+    //     planeX = planeX * Math.cos(rotateSpeed) - planeY * Math.sin(rotateSpeed);
+    //     planeY = oldPlaneX * Math.sin(rotateSpeed) + planeY * Math.cos(rotateSpeed);
+    // }
 
     // divided by 100 because raw values would be far too high
     // also fixes mouseMovementX sometimes getting stuck at a non 0 value when you stop moving the mouse by repeatedly dividing the value
